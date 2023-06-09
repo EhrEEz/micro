08436ae77ca71d516b037b32b705d854c9b47b5c
EhrEEz <xtutorials66@gmail.com>
2023-06-09 18:14:14 +0545

    Initial Commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import '../scss/_app.scss';
+</script>
+
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Spline+Sans:wght@300;400;500;600;700&display=swap"
+		rel="stylesheet"
+	/>
+	<link
+		rel="stylesheet"
+		href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20,200,1,0"
+	/>
+	<style>
+		.material-symbols-rounded {
+			font-size: 20px;
+			font-variation-settings: 'FILL' 1, 'wght' 200, 'GRAD' 0, 'opsz' 16;
+		}
+	</style>
+</svelte:head>
+
+<main>
+	<slot />
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<header>
+	<nav class="glass">
+		<h1 class="nav--title"><span class="larger">Ares</span> Shrestha</h1>
+		<div class="rt-wrapper">
+			<button class="slab">
+				<span class="slab-content">
+					<span class="material-symbols-rounded"> settings </span>
+				</span>
+			</button>
+		</div>
+	</nav>
+</header>
+<section class="hero__section">
+	<div class="hero__bg">
+		<!-- svelte-ignore a11y-media-has-caption -->
+		<video autoplay playsinline loop>
+			<source type="video/mp4" src="/src/static/hero-video.mp4" />
+		</video>
+	</div>
+	<div class="hero__text--wrapper" />
+</section>
